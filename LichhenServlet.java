c78352add3d4e0315b96450fe01a4aee8bf1f5f1
NGO-PTIT <122855056+NGO-PTIT@users.noreply.github.com>
2023-11-14 21:21:01 +0700

    Update LichhenServlet.java

--- LichhenServlet.java
+++ LichhenServlet.java
@@ -1,8 +1,13 @@
 package DKLH;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,12 +17,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/")
-public class LichhenServlet extends HttpServlet {
-    private LichhenDAO lichhenDAO;
+public class LichhenDAO extends HttpServlet {
     
-    public void init(){
-       lichhenDAO = new LichhenDAO();
-   }
+    private String jdbcURL = "jdbc:mysql://localhost:3306/demopttk?useSSL=false";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "17032002";
+    
+    private static final String INSERT_lichhen_SQL = "INSERT INTO lich_hen" + "  (id, ngay, gio, ten, email, dienthoai, ghichu) VALUES " + " (?, ?, ?, ?, ?, ?, ?);";
+    private static final String SELECT_lichhen_SQL = "select ngay, gio, ten, email, dienthoai, ghichu from lich_hen where id = ?";
+    private static final String SELECT_ALL_lichhen_SQL = "select * from lich_hen";
+    private static final String UPDATE_lichhen_SQL = "update lich_hen set ngay =?, gio=?, ten=?, email=?, dienthoai=?, ghichu=?  where id = ?;";
+
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -61,7 +71,7 @@ public class LichhenServlet extends HttpServlet {
         
     public void listLH(HttpServletRequest request, HttpServletResponse response)
     throws SQLException, IOException, ServletException {
-        List < Lichhen > listLH = lichhenDAO.selectAllLH();
+        List < Lichhen > listLH = selectAllLH();
         request.setAttribute("listLH", listLH);
         RequestDispatcher dispatcher = request.getRequestDispatcher("danhsachlichhen.jsp");
         dispatcher.forward(request, response);
@@ -77,7 +87,7 @@ public class LichhenServlet extends HttpServlet {
     throws SQLException, ServletException, IOException {
         String STT = request.getParameter("id");
         Integer id = Integer.valueOf(STT);
-        Lichhen existingLH = lichhenDAO.selectLH(id);
+        Lichhen existingLH = selectLH(id);
         RequestDispatcher dispatcher = request.getRequestDispatcher("lichhen-form.jsp");
         request.setAttribute("lh", existingLH);
         dispatcher.forward(request, response);
@@ -93,12 +103,12 @@ public class LichhenServlet extends HttpServlet {
         String phone = request.getParameter("dienthoai");
         String note = request.getParameter("ghichu");
         
-        List < Lichhen > listLH = lichhenDAO.selectAllLH();
+        List < Lichhen > listLH = selectAllLH();
         Integer id = listLH.size() + 1;
 
         Lichhen appointment = new Lichhen(id, date, time, name, email, phone, note);
         
-        lichhenDAO.insertAppointment(appointment);
+        insertAppointment(appointment);
         response.sendRedirect("datthanhcong.jsp");
     }
     
@@ -114,8 +124,108 @@ public class LichhenServlet extends HttpServlet {
         
         Lichhen newappointment = new Lichhen(1, date, time, name, email, phone, note);
 
-        lichhenDAO.updateLH(newappointment);
+        updateLH(newappointment);
         response.sendRedirect("/PTTK1/list");
+    }
+    
+    protected  Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    
+    public void insertAppointment(Lichhen appointment) throws SQLException {
+
+        try (Connection connection = getConnection(); 
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_lichhen_SQL)) {
+            
+            preparedStatement.setInt(1, appointment.getId());
+            preparedStatement.setString(2, appointment.getNgay());
+            preparedStatement.setString(3, appointment.getGio());
+            preparedStatement.setString(4, appointment.getTenKH());
+            preparedStatement.setString(5, appointment.getEmail());
+            preparedStatement.setString(6, appointment.getDienthoai());
+            preparedStatement.setString(7, appointment.getGhichu());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Lichhen selectLH(Integer ID) {
+        Lichhen lh = null;
+        try (Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_lichhen_SQL);) {
+            preparedStatement.setInt(1, ID);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                
+                String ngay = rs.getString("ngay");
+                String gio= rs.getString("gio");
+                String tenKH = rs.getString("ten");
+                String email = rs.getString("email");
+                String dienthoai = rs.getString("dienthoai");
+                String ghichu = rs.getString("ghichu");
+
+                lh = new Lichhen(ID, ngay, gio, tenKH, email, dienthoai, ghichu);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lh;
+    }
+    
+    public List < Lichhen > selectAllLH() {
+
+        List < Lichhen > lhlist = new ArrayList < > ();
+        
+        try (Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_lichhen_SQL);) {
+            
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                Integer id = rs.getInt("id");
+                String ngay = rs.getString("ngay");
+                String gio= rs.getString("gio");
+                String tenKH = rs.getString("ten");
+                String email = rs.getString("email");
+                String dienthoai = rs.getString("dienthoai");
+                String ghichu = rs.getString("ghichu");
+                
+                lhlist.add(new Lichhen(id, ngay, gio, tenKH, email, dienthoai, ghichu));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lhlist;
+    }
+    
+    public boolean updateLH(Lichhen lh) throws SQLException {
+        boolean rowUpdated;
+        try (Connection connection = getConnection(); 
+                PreparedStatement statement = connection.prepareStatement(UPDATE_lichhen_SQL)){
+
+            statement.setString(1, lh.getNgay());
+            statement.setString(2, lh.getGio());
+            statement.setString(3, lh.getTenKH());
+            statement.setString(4, lh.getEmail());
+            statement.setString(5, lh.getDienthoai());
+            statement.setString(6, lh.getGhichu());
+            statement.setInt(7, lh.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
     }
 
 }
